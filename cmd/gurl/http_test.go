837ae8a72ad70219f15d2cd9610a91269860861a
Ren Ogaki <re.yuz77777@gmail.com>
2021-04-16 19:03:40 +0900

    Fix tests

--- cmd/gurl/http_test.go
+++ cmd/gurl/http_test.go
@@ -9,37 +9,40 @@ import (
 )
 
 func TestMakeHeaderFromDefaultHeader(t *testing.T) {
-	cf := defaultHeaders{
-		"127.0.0.1:8080": {
+	dhs := defaultHeaders{
+		"localhost:8080": {
 			"Accept-Language": "en-US",
 			"Content-Type":    "application/json",
 		},
-		"127.0.0.1:8080/v1/*": {
+		"localhost:8080/v1/*": {
 			"Authorization": "Basic foo",
 			"Content-Type":  "x-www-form-urlencoded",
 		},
-		"127.0.0.1:8080/v1/foo": {
+		"localhost:8080/v1/bar": {
 			"Accept-Charset": "utf-8",
 			"Authorization":  "Basic bar",
 		},
-		"127.0.0.1:8888": {
+		"localhost:8888": {
 			"Content-Type": "text/plain",
 		},
 	}
 
 	tests := []struct {
+		name     string
 		url      string
 		expected http.Header
 	}{
 		{
-			url: "http://127.0.0.1:8080/v2/foo",
+			name: "set header",
+			url:  "http://localhost:8080/v2/foo",
 			expected: http.Header{
 				"Accept-Language": {"en-US"},
 				"Content-Type":    {"application/json"},
 			},
 		},
 		{
-			url: "http://127.0.0.1:8080/v1/bar",
+			name: "the default header for the deeper path has priority",
+			url:  "http://localhost:8080/v1/foo",
 			expected: http.Header{
 				"Accept-Language": {"en-US"},
 				"Authorization":   {"Basic foo"},
@@ -47,7 +50,8 @@ func TestMakeHeaderFromDefaultHeader(t *testing.T) {
 			},
 		},
 		{
-			url: "http://127.0.0.1:8080/v1/foo",
+			name: "the default header for the more detailed path has priority",
+			url:  "http://localhost:8080/v1/bar",
 			expected: http.Header{
 				"Accept-Charset":  {"utf-8"},
 				"Accept-Language": {"en-US"},
@@ -55,28 +59,22 @@ func TestMakeHeaderFromDefaultHeader(t *testing.T) {
 				"Content-Type":    {"x-www-form-urlencoded"},
 			},
 		},
-		{
-			url: "http://127.0.0.1:8080/v1/foo/bar",
-			expected: http.Header{
-				"Accept-Language": {"en-US"},
-				"Authorization":   {"Basic foo"},
-				"Content-Type":    {"x-www-form-urlencoded"},
-			},
-		},
 	}
 
 	for _, tt := range tests {
-		uri, err := url.Parse(tt.url)
-		if err != nil {
-			t.Fatalf("parsing tt.url failed. err=%s", err.Error())
-		}
-		got, err := makeHeaderFromDefaultHeader(uri, cf)
-		if err != nil {
-			t.Fatalf("makeHeaderFromDefaultHeader failed. url=%s, err=%s", tt.url, err)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			uri, err := url.Parse(tt.url)
+			if err != nil {
+				t.Errorf("parsing tt.url failed. err=%s", err.Error())
+			}
+			got, err := makeHeaderFromDefaultHeader(uri, dhs)
+			if err != nil {
+				t.Errorf("makeHeaderFromDefaultHeader failed. url=%s, err=%s", tt.url, err)
+			}
 
-		if !cmp.Equal(got, tt.expected) {
-			t.Errorf("makeHeaderFromDefaultHeader wrong. got=%v, expected=%v", got, tt.expected)
-		}
+			if !cmp.Equal(got, tt.expected) {
+				t.Errorf("makeHeaderFromDefaultHeader wrong. got=%v, expected=%v", got, tt.expected)
+			}
+		})
 	}
 }
